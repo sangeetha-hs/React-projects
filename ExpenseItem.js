77086cc7cc65879b4718ba2267fc53b424715c67
sangeetha-hs <118202947+sangeetha-hs@users.noreply.github.com>
2023-09-14 13:41:30 +0530

    Create ExpenseItem.js

---
+++ ExpenseItem.js
@@ -0,0 +1,17 @@
+function ExpenseItem(){
+    return(
+        <>
+        <div>
+            
+        <h2>Expense item!</h2>
+        <h1>Food</h1>
+        <h3> $ 10</h3>
+        <h1>Petrol</h1>
+        <h3> $ 100</h3>
+        <h1>Movies</h1>
+        <h3> $ 200</h3>
+        </div>
+ </>
+    )
+}
+export default ExpenseItem;
